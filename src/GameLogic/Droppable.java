16f205709350b10c76848cd876b2a845228632c7
allan <allanjiang17@gmail.com>
2021-05-13 12:43:41 -0700

    UI version 2.0

--- src/GameLogic/Droppable.java
+++ src/GameLogic/Droppable.java
@@ -2,19 +2,19 @@ package GameLogic;
 
 import java.awt.*;
 
-public class Boundary extends Sprite {
-    public Boundary(int x, int y, int width, int height) {
+public class Droppable extends Sprite{
+
+    public Droppable(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
 
     @Override
     public void update(GameManager manager) {
-        //do nothing
+
     }
 
     @Override
     public void render(Graphics g) {
-        //do nothing
+
     }
 }
-
